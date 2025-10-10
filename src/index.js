6da8e34bbc1016df24ccc92866682e10a99a8dec
Vano3421 <vanya.matyash.22@gmail.com>
2025-10-10 13:51:27 +0200

    v0.0.1a

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+const typeDefs = `#graphql   
+
+    type Book {
+        title: String
+        author: String
+    }
+
+    type Query {
+        books: [Book]
+    }
+`;
